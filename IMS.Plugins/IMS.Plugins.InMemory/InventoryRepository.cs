76f1af3f2c6d10098a8af5474689b703a057b867
Bruno Pereira <bsallesp@gmail.com>
2023-02-13 08:37:28 -0500

    Add project files.

---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+	public class InventoryRepository : IInventoryRepository
+	{
+		private List<Inventory> _inventories;
+
+		public InventoryRepository()
+		{
+			_inventories = new List<Inventory>()
+			{
+				new Inventory { InventoryID = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+				new Inventory { InventoryID = 1, InventoryName = "Bike Body", Quantity = 20, Price = 15 },
+				new Inventory { InventoryID = 1, InventoryName = "Abajour", Quantity = 50, Price = 111 },
+				new Inventory { InventoryID = 1, InventoryName = "Bike Abajour", Quantity = 10, Price = 9 },
+				new Inventory { InventoryID = 1, InventoryName = "Abajour Seat", Quantity = 3, Price = 6 },
+				new Inventory { InventoryID = 1, InventoryName = "Bike Moto", Quantity = 10, Price = 8 }
+			};
+		}
+
+		public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+		{
+			if (string.IsNullOrWhiteSpace(name)) return await Task.FromResult(_inventories);
+
+			return _inventories.Where(x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+		}
+	}
+}
